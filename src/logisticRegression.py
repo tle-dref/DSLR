7b04f77a36a2e2a7997fd4907fdab07eff02f713
tle-dref <thomas.ledref@gmail.com>
2025-02-11 17:00:15 +0100

    ADD: logisticRegression.py with the model

---
+++ src/logisticRegression.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class LogisticRegression():
+    def __init__(self, l_rate = 0.001, nb_iters = 1000):
+        self.l_rate = l_rate
+        self.nb_iters = nb_iters
+        self.weights = None
+        self.bias = None
+
+    def sigmoid(self, x):
+        np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self, X, y):
+        nb_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.nb_iters):
+            # formula for logistic regression
+            linear_pred = np.dot(X, self.weights) + self.bias
+            prediction = self.sigmoid(linear_pred)
+
+            # gradient for weights and bias
+            dw = (1 / nb_samples) * np.dot(X.T, (prediction - y))
+            db = (1 / nb_samples) * np.sum(prediction - y)
+
+            # update weights and bias
+            self.weights -= self.l_rate * dw
+            self.bias -= self.l_rate * db
+
+    def predict(self, X):
+        linear_pred = np.dot(X, self.weights) + self.bias
+        y_pred = self.sigmoid(linear_pred)
+        class_pred = [0 if y < 0.5 else 1 for y in y_pred]
+        return class_pred
